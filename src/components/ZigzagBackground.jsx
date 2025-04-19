2ea294a4f6b9de20b6c9073b623bc1f4a0d5f502
juanchoalcoba <juanchoalcoba@gmail.com>
2025-04-19 14:50:18 -0300

    lenis installation

--- src/components/ZigzagBackground.jsx
+++ src/components/ZigzagBackground.jsx
@@ -39,7 +39,7 @@ const ZigzagBackground = () => {
   return (
     <div
       ref={zigzagRef}
-      className="fixed top-0 left-1/2 w-50 h-50 bg-cover bg-no-repeat drop-shadow-2xl rounded-full z-[-10] opacity-20 pointer-events-none"
+      className="fixed top-0 left-1/2 w-170 h-170 bg-cover bg-no-repeat drop-shadow-2xl rounded-full z-[-10] opacity-10 pointer-events-none"
       style={{
         transform: 'translateX(-80%)',
         backgroundImage: 'url(/luna.png)', // Reemplaza con la ruta correcta de la imagen
