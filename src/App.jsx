260185ee344982c75afe9d6f4db297dcc81a7920
juanchoalcoba <juanchoalcoba@gmail.com>
2025-04-19 13:56:58 -0300

    lenis installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BookingsPage from './pages/BookingsPage';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 function App() {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      smoothWheel: true,
+      smoothTouch: true,
+    });
+  
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+  
+    requestAnimationFrame(raf);
+  
+    // Forzar el manejo de las teclas del teclado para el scroll
+    const handleKeydown = (event) => {
+      if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+        lenis.scrollTo(window.scrollY + (event.key === 'ArrowDown' ? 100 : -100), { duration: 1 });
+        event.preventDefault();
+      }
+    };
+  
+    // Escuchar eventos de teclado
+    window.addEventListener('keydown', handleKeydown);
+  
+    // Limpieza de los eventos cuando el componente se desmonta
+    return () => {
+      lenis.destroy();
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
+
+
+
   return (
     <BrowserRouter>
       <Routes>
