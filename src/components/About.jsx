1c6910848ca1a6915800b70f65353fb189eb223b
juanchoalcoba <juanchoalcoba@gmail.com>
2025-04-20 00:51:42 -0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,16 +39,16 @@ const About = () => {
       className="relative w-full flex flex-col justify-center items-center py-24 px-6 md:px-12"
     >
       <div className="relative z-10 flex flex-col justify-center items-center w-full">
-        <div className="flex flex-col md:flex-row justify-evenly gap-24 items-center">
+        <div className="flex flex-col md:flex-row justify-evenly gap-12 items-center">
           {/* Texto con animación */}
           <div
             ref={textRef}
             className="text-center md:text-left flex flex-col justify-start items-center md:items-start max-w-xl"
           >
-            <h1 className="text-[42px] md:text-[50px] drop-shadow-[2px_2px_2px_rgba(0,0,0,0.8)] font-bold text-blue-100">
+            <h1 className="text-[22px] md:text-[50px] drop-shadow-[2px_2px_2px_rgba(0,0,0,0.8)] font-bold text-blue-100">
               Acerca de Nosotros
             </h1>
-            <p className="w-96 text-lg md:text-xl font-light text-gray-200 drop-shadow-[2px_4px_6px_rgba(0,0,0,0.2)] mt-4">
+            <p className="w-96 text-md md:text-xl font-light text-gray-200 drop-shadow-[2px_4px_6px_rgba(0,0,0,0.2)] mt-4">
               En nuestra cervecería, no solo servimos cerveza artesanal: creamos
               una experiencia inolvidable. Cada sorbo cuenta una historia,
               elaborada con pasión, ingredientes seleccionados y técnicas
